d44a12880b7d80e3ed287ace9e60b3a5a25747de
Karla Bravo <110432456+KarlaVBM@users.noreply.github.com>
2022-08-11 16:19:19 -0500

    Update position.js

--- js/position.js
+++ js/position.js
@@ -34,3 +34,58 @@ $("#contact").on('click', function (e){
         }, 2000);
     return false;
 });
+
+
+$("#home2").on('click', function (e) {
+    e.preventDefault();
+    //Subir el scroll arriba
+    $(".cont-full-MinMenu").css({ 'display': 'none' });
+    $("body").css({ 'overflow-y': 'scroll' });
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+}); 
+
+$("#info2").on('click', function (e) {
+    e.preventDefault();
+    //Subir el scroll arriba
+    $(".cont-full-MinMenu").css({ 'display': 'none' });
+    $("body").css({ 'overflow-y': 'scroll' });
+    $('html, body').animate({
+        scrollTop: $(".cont-full-info").offset().top - 70
+        }, 2000);
+    return false;
+}); 
+
+$('#habilidades2').on('click', function (e){
+    e.preventDefault();
+    /*$("html, body").animate({ scrollTop: 900-70 }, "slow");*/
+    $(".cont-full-MinMenu").css({ 'display': 'none' });
+    $("body").css({ 'overflow-y': 'scroll' });
+    $('html, body').animate({
+        scrollTop: $(".cont-full-skills").offset().top - 70
+        }, 2000);
+    return false;
+} );
+
+$('#briefcase2').on('click', function (e){
+    e.preventDefault();
+    $(".cont-full-MinMenu").css({ 'display': 'none' });
+    $("body").css({ 'overflow-y': 'scroll' });
+    $('html, body').animate({
+        scrollTop: $(".cont-full-briefcase").offset().top - 70
+        }, 2000);
+    return false;
+} );
+
+$("#contact2").on('click', function (e){
+    e.preventDefault();
+    $(".cont-full-MinMenu").css({ 'display': 'none' });
+    $("body").css({ 'overflow-y': 'scroll' });
+    $('html, body').animate({
+        scrollTop: $(".cont-full-contact").offset().top - 70
+        }, 2000);
+    return false;
+});
+
+
+
